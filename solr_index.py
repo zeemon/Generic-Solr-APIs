21404ce55717a71a6ca2414b312b684ac070f61f
naizy <naizy@ajency.in>
2017-03-14 22:15:33 +0530

    solr filtering using faceted queries

--- solr_index.py
+++ solr_index.py
@@ -1,28 +1,49 @@
-import urllib2
+import urllib2, urllib
 import json
 
 class SolrIndex(object):
 
 	SOLR_URL = 'http://localhost:8983/solr/'
 
-	def __init__(self, core, id):
+	def __init__(self, core):
 		self.core = core
-		self.id = id
 
-	def get_data(self):
-		endpoint= "/get?id="+str(self.id)
+	def get_data(self, id):
+		endpoint= "/get?id="+str(id)
 		req = self.__solr_request_object(endpoint)
 		response = urllib2.urlopen(req)
 		data= self.__solr_response_data(response)
-		if not data: data= {'id': self.id}
+		if not data: data= {'id': id}
 		return data
 
-	def update_data(self,new_data):
-		data= self.get_data()
+	def update_data(self,new_data, id):
+		data= self.get_data(id)
 		for k,v in new_data.items():
 			data[k]=v
 		data= json.dumps([data])
 		return self.__save_to_solr(data)
+
+	def search(self, fl=[], fq={}, rows=10):
+
+		params = {
+			'query' : '&q=*:*',
+			'fl'	: '&fl='+','.join(fl) if fl else '',
+			'rows'  : '&rows='+ str(rows),
+			'fq'	: self.__get_fq_params(fq) if fq else ''
+		}
+
+		endpoint= "/select?%s&wt=json&indent=true" % ''.join(params.values())
+
+		req = self.__solr_request_object(endpoint)
+		response = urllib2.urlopen(req)
+		data= json.loads(response.read())
+		return data['response']
+
+	def __get_fq_params(self, fq):
+		fq_params= ''
+		for key, value in fq.items():
+			fq_params +='&fq=%s:"%s"' % (key, urllib.quote_plus(value))
+		return fq_params
 
 	def __solr_request_object(self, endpoint):
 		full_url= self.SOLR_URL+self.core+endpoint
--- tests.py
+++ tests.py
@@ -4,10 +4,22 @@ import unittest
 
 class Test_SolrIndex(unittest.TestCase):
 	def test_SolrUpdateIndex(self):
-		solr_obj = SolrIndex('blogs',419)
+		solr_obj = SolrIndex('blogs')
+		blog_id=419
 		new_data= {
-			'title': '7 signs that you are the ulimate Bolywood Bride'
+			'title': '10 signs that you are the ulimate Bolywood Bride'
 		}
-		solr_obj.update_data(new_data)
-		data= solr_obj.get_data()
+		solr_obj.update_data(new_data,blog_id)
+		data= solr_obj.get_data(blog_id)
 		self.assertEqual(new_data['title'], data['title'])
+
+	def test_SolrSearch(self):
+		solr_obj = SolrIndex('blogs')
+		blogs = solr_obj.search(
+			fl=['id','title','slug'],
+			fq={
+				'title': '10 signs'
+			},
+			rows=10
+		)
+		self.assertTrue(len(blogs)>=1)
